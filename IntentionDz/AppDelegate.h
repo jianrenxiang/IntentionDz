2dcbb43016a2eb8c3e101267b2218ff301597855
mrsLiu <1479055311@qq.com>
2016-11-14 09:17:12 +0800

    Initial Commit

---
+++ IntentionDz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntentionDz
+//
+//  Created by EWSAPPLE on 16/11/14.
+//  Copyright © 2016年 com.ews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntentionDz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntentionDz
+//
+//  Created by EWSAPPLE on 16/11/14.
+//  Copyright © 2016年 com.ews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
